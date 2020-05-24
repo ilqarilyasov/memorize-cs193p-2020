4e319d23063f705f57de74dc31fcebbd55f28ad5
Ilgar Ilyasov <iilyasov@awarex.com>
2020-05-24 16:58:52 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Ilqar Ilyasov on 5/24/20.
+//  Copyright © 2020 IIlyasov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
